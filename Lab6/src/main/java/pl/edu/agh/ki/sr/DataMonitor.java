a5a29bbc914c7c1c3e284a80b3cedd7c675268a7
19mariusz95 <19mariusz95@gmail.com>
2017-06-04 13:28:01 +0200

    lab6

---
+++ Lab6/src/main/java/pl/edu/agh/ki/sr/DataMonitor.java
@@ -0,0 +1,103 @@
+package pl.edu.agh.ki.sr;
+
+import org.apache.zookeeper.AsyncCallback.StatCallback;
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.KeeperException.Code;
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.data.Stat;
+
+import java.util.Arrays;
+
+public class DataMonitor implements Watcher, StatCallback {
+    private ZooKeeper zk;
+    private String znode;
+    private Watcher chainedWatcher;
+    private DataMonitorListener listener;
+
+    byte prevData[];
+
+    public DataMonitor(ZooKeeper zk, String znode, Watcher chainedWatcher,
+                       DataMonitorListener listener) throws KeeperException, InterruptedException {
+        this.zk = zk;
+        this.znode = znode;
+        this.chainedWatcher = chainedWatcher;
+        this.listener = listener;
+        zk.exists(znode, true, this, null);
+    }
+
+    public interface DataMonitorListener {
+        /**
+         * The existence status of the node has changed.
+         */
+        void exists(byte data[]);
+
+        /**
+         * The ZooKeeper session is no longer valid.
+         *
+         * @param rc
+         *                the ZooKeeper reason code
+         */
+        void closing(int rc);
+    }
+
+    public void process(WatchedEvent event) {
+        String path = event.getPath();
+        if (event.getType() == Event.EventType.None) {
+            switch (event.getState()) {
+                case SyncConnected:
+                    break;
+                case Expired:
+                    listener.closing(KeeperException.Code.SessionExpired);
+                    break;
+            }
+        } else if(event.getType() == Event.EventType.NodeChildrenChanged) {
+            try {
+                System.out.println(zk.getChildren(znode, this).size());
+            } catch (KeeperException | InterruptedException e) {
+                e.printStackTrace();
+            }
+        } else {
+            if (path != null && path.equals(znode)) {
+                zk.exists(znode, true, this, null);
+            }
+        }
+        if (chainedWatcher != null) {
+            chainedWatcher.process(event);
+        }
+    }
+
+    public void processResult(int rc, String path, Object ctx, Stat stat) {
+        boolean exists;
+        switch (rc) {
+            case Code.Ok:
+                exists = true;
+                break;
+            case Code.NoNode:
+                exists = false;
+                break;
+            case Code.SessionExpired:
+            case Code.NoAuth:
+                listener.closing(rc);
+                return;
+            default:
+                zk.exists(znode, true, this, null);
+                return;
+        }
+
+        byte b[] = null;
+        if (exists) {
+            try {
+                b = zk.getData(znode, false, null);
+            } catch (KeeperException | InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        if ((b == null && b != prevData)
+                || (b != null && !Arrays.equals(prevData, b))) {
+            listener.exists(b);
+            prevData = b;
+        }
+    }
+}
---
+++ Lab6/src/main/java/pl/edu/agh/ki/sr/Executor.java
@@ -0,0 +1,146 @@
+package pl.edu.agh.ki.sr;
+
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class Executor implements Watcher, Runnable, DataMonitor.DataMonitorListener {
+    private DataMonitor dm;
+    private ZooKeeper zk;
+    private final String znode;
+    private String[] exec;
+    private Process child;
+
+    public Executor(String hostPort, String znode, String exec[]) throws KeeperException, IOException, InterruptedException {
+        this.znode = znode;
+        this.exec = exec;
+        zk = new ZooKeeper(hostPort, 10000, this);
+        dm = new DataMonitor(zk, znode, null, this);
+
+    }
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        if (args.length < 2) {
+            System.err
+                    .println("USAGE:hostPort program [args ...]");
+            System.exit(2);
+        }
+        String hostPort = args[0];
+        String znode = "/znode_testowy";
+        String exec[] = new String[args.length - 1];
+        System.arraycopy(args, 1, exec, 0, exec.length);
+        try {
+            new Executor(hostPort, znode, exec).run();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void process(WatchedEvent event) {
+        dm.process(event);
+    }
+
+    public void run() {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            switch (line) {
+                case "tree":
+                    try {
+                        show(znode, znode, 0);
+                    } catch (KeeperException | InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+            }
+        }
+    }
+
+    private void show(String child, String path, int i) throws KeeperException, InterruptedException {
+        IntStream.rangeClosed(0, i).forEach(k -> System.out.print(" "));
+        List<String> children = zk.getChildren(path, dm);
+        System.out.println(child);
+        children.forEach(c -> {
+            try {
+                show(c, path + "/" + c, i + 1);
+            } catch (KeeperException | InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+    }
+
+    public void closing(int rc) {
+        synchronized (this) {
+            notifyAll();
+        }
+    }
+
+    static class StreamWriter extends Thread {
+        OutputStream os;
+
+        InputStream is;
+
+        StreamWriter(InputStream is, OutputStream os) {
+            this.is = is;
+            this.os = os;
+            start();
+        }
+
+        public void run() {
+            byte b[] = new byte[80];
+            int rc;
+            try {
+                while ((rc = is.read(b)) > 0) {
+                    os.write(b, 0, rc);
+                }
+            } catch (IOException ignored) {
+            }
+
+        }
+    }
+
+    public void exists(byte[] data) {
+        if (data == null) {
+            if (child != null) {
+                System.out.println("Killing process");
+                child.destroyForcibly();
+                child.destroy();
+                try {
+                    child.waitFor();
+                } catch (InterruptedException ignored) {
+                }
+            }
+            child = null;
+        } else {
+            if (child != null) {
+                System.out.println("Stopping child");
+                child.destroy();
+                try {
+                    child.waitFor();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            try {
+                child = Runtime.getRuntime().exec(exec);
+                System.out.println("Starting child ");
+                new StreamWriter(child.getInputStream(), System.out);
+                new StreamWriter(child.getErrorStream(), System.err);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
